f0d3360a36ad56a05f769b25f0c0888cc0136b7d
Kelvin Makamu <makamu3pmkelvin@gmail.com>
2021-01-21 19:40:30 +0300

    Modified login page to move images to stylesheets

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  API_URL: `https://itnps-backend.herokuapp.com/api/v1`,
+  API_URL: `https://itnps-backend.herokuapp.com/api/v1/`,
   DEFAULT_FILTER_START_DATE: '2020-11-01',
   DEFAULT_FILTER_END_DATE: '2020-11-08',
   responsesApiEndpoint: 'https://itnps-backend.herokuapp.com/api/v1/responses',
